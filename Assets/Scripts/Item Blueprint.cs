000e40cbdbc822267d9cbce7c922e4fb1fd60d3b
tomaszkula <tomkul.priv@gmail.com>
2019-09-04 19:12:36 +0200

    Initial commit

---
+++ Assets/Scripts/Item Blueprint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item Blueprint")]
+public class ItemBlueprint : ScriptableObject
+{
+    [SerializeField] GameObject itemPrefab;
+    [SerializeField] Vector2 size;
+    [SerializeField] int cost;
+    [SerializeField] int exp;
+
+    public GameObject GetItemPrefab() { return itemPrefab; }
+
+    public Vector2 GetSize() { return size; }
+
+    public int GetCost() { return cost; }
+
+    public int GetExp() { return exp; }
+}
