41d01936a145c586cdfdf04c5d4222e592cbc46c
tomaszkula <tomkul.priv@gmail.com>
2019-09-09 22:30:46 +0200

    v0.0.5
    ShopSystem againnnnn
    Now it's rly prepared for tabs in the shop. THAT'S IT
    - buttons which works like sliders
    - lists of lists in the inspector for shop items
    - shop grid for items has been improved

--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -4,43 +4,66 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     [SerializeField] GameObject itemHolderPrefab;
-    [SerializeField] Transform grid;
 
     [Header("Item Lists")]
-    [SerializeField] List<ShopItem> plants;
+    [SerializeField] Transform itemsGridsParent;
+    [SerializeField] GameObject itemsGrid;
+    [SerializeField] List<ShopItemsList> itemsLists = new List<ShopItemsList>();
 
     [Header("Item Lists View Sliders")]
     [SerializeField] GameObject leftSlider;
     [SerializeField] GameObject rightSlider;
 
-    int currentPage = 1;
-    int plantPages;
+    List<GameObject> grids = new List<GameObject>();
+    List<int> pagesCount = new List<int>();
+    int currentItemsListId, currentPage;
+    
 
     void Start()
     {
         FillShop();
+        OpenTab();
     }
 
     void FillShop()
     {
-        foreach (ShopItem plant in plants)
+        for(int i = 0; i < itemsLists.Count; i++)
         {
-            GameObject itemHolder = (GameObject)Instantiate(itemHolderPrefab, grid);
-            ItemHolder holderScript = itemHolder.GetComponent<ItemHolder>();
-            holderScript.SetItemMainInfo(plant.GetItemName(), plant.GetItemIcon());
-            holderScript.SetItemInfo(plant.GetItemExp(), plant.GetItemTime(), plant.GetItemPrize(), plant.GetItemAdditionalPrize());
-            holderScript.SetItemCost(plant.GetItemCoinCost(), plant.GetItemDollarCost());
-        }
+            ShopItemsList items = itemsLists[i];
 
-        plantPages = Mathf.CeilToInt(plants.Count / 8.0f);
+            grids.Add(Instantiate(itemsGrid, itemsGridsParent));
+            grids[i].SetActive(false);
+
+            foreach (ShopItem item in items)
+            {
+                GameObject itemHolder = (GameObject)Instantiate(itemHolderPrefab, grids[i].transform);
+                ItemHolder holderScript = itemHolder.GetComponent<ItemHolder>();
+                holderScript.SetItemMainInfo(item.GetItemName(), item.GetItemIcon());
+                holderScript.SetItemInfo(item.GetItemExp(), item.GetItemTime(), item.GetItemPrize(), item.GetItemAdditionalPrize());
+                holderScript.SetItemCost(item.GetItemCoinCost(), item.GetItemDollarCost());
+            }
+
+            int pages = Mathf.CeilToInt(items.Count / 8.0f);
+            pagesCount.Add(pages);
+        }
+    }
+
+    void OpenTab(int idList = 0, int idPage = 1)
+    {
+        Debug.Log(currentItemsListId);
+        currentItemsListId = idList;
+        currentPage = idPage;
+
+        grids[currentItemsListId].SetActive(true);
+
         CheckSliders();
     }
 
     public void SlideGridView(int side)
     {
-        Vector3 newPos = grid.localPosition;
+        Vector3 newPos = grids[currentItemsListId].transform.localPosition;
         newPos.y += side * 735f;
-        grid.localPosition = newPos;
+        grids[currentItemsListId].transform.localPosition = newPos;
 
         currentPage += side;
         CheckSliders();
@@ -51,7 +74,7 @@ public class Shop : MonoBehaviour
         if (currentPage > 1) leftSlider.SetActive(true);
         else leftSlider.SetActive(false);
 
-        if (currentPage < plantPages) rightSlider.SetActive(true);
+        if (currentPage < pagesCount[currentItemsListId]) rightSlider.SetActive(true);
         else rightSlider.SetActive(false);
     }
 
---
+++ Assets/ShopItemsList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ShopItemsList
+{
+    public List<ShopItem> items;
+
+    public int Count
+    {
+        get
+        {
+            return items.Count;
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new ShopItemsList.Enumerator(items);
+    }
+
+    private sealed class Enumerator : IEnumerator
+    {
+        private List<ShopItem> outer;
+        private int currentIndex = -1;
+
+        public object Current
+        {
+            get { return outer[currentIndex]; }
+        }
+
+        internal Enumerator(List<ShopItem> outer)
+        {
+            this.outer = outer;
+        }
+
+        public bool MoveNext()
+        {
+            int childCount = outer.Count;
+            return ++currentIndex < childCount;
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1;
+        }
+    }
+}
